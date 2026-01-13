4d83d4595e9418c0f389bc68b9a0c115eb57fea1
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2026-01-13 08:52:13 +0600

    Update quiz-page.blade.php

--- resources/views/livewire/quiz-page.blade.php
+++ resources/views/livewire/quiz-page.blade.php
@@ -105,7 +105,7 @@
                             <button wire:click="submitQuiz"
                                 {{ !isset($answers[$currentQuestion]) ? 'disabled' : '' }}
                                 class="px-6 py-2 bg-green-600 text-white font-medium rounded-lg hover:bg-green-700 disabled:opacity-50 disabled:cursor-not-allowed transition-colors">
-                                Submit Quiz
+                                Find My Brokers →
                             </button>
                         @endif
                     </div>
