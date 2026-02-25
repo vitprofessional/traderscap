10d4eb426b1b2b9dbe0fd69bf44fe9a012cd4c94
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2026-02-25 13:56:14 +0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
