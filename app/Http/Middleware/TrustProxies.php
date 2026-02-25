dcf2e7058ebc5731aca027b0ea6b0aca02f42eb6
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2026-02-25 12:50:42 +0600

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,11 +3,16 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
     protected $proxies = '*';
 
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    // Use Symfony constants (compatible)
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
 }
